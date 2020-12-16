edc66f240a6389aef27a60a22423cf43fb820f7c
Abdul Momin <abdulmomin114477@gmail.com>
2020-12-16 10:52:49 +0600

    finished setting up the project

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,31 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-   entry: {
-      main: "./src/js/index.js",
-      vendor: "./src/js/vendor.js",
-   },
-   module: {
-      rules: [
-         {
-            test: /\.html$/,
-            use: ["html-loader"],
-         },
-         {
-            test: /\.(svg|png|jpg|gif)$/,
-            use: {
-               loader: "file-loader",
-               options: {
-                  outputPath: "img",
-                  name: "[name].[hash].[ext]",
-               },
-            },
-         },
-      ],
-   },
+  entry: './src/ts/app.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(svg|ico|png|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'img',
+            name: '[name].[hash].[ext]',
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,42 +1,42 @@
-const path = require("path");
-const common = require("./webpack.common");
-const merge = require("webpack-merge");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const common = require('./webpack.common');
+const merge = require('webpack-merge');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
-   mode: "development",
-   output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "js/[name].js",
-   },
-   plugins: [
-      new HtmlWebpackPlugin({
-         filename: "index.html",
-         template: "./src/template.html",
-      }),
-   ],
-   devServer: {
-      contentBase: "./dist",
-      port: 8080,
-      open: {
-         app: ["chrome", "--incognito"],
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/template.html',
+    }),
+  ],
+  devServer: {
+    contentBase: './dist',
+    port: 3000,
+    open: {
+      app: ['google-chrome', '--incognito'],
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
       },
-   },
-   module: {
-      rules: [
-         {
-            test: /\.js$/,
-            enforce: "pre",
-            use: ["source-map-loader"],
-         },
-         {
-            test: /\.scss$/,
-            use: [
-               "style-loader", //3. Inject styles into DOM
-               "css-loader", //2. Turns css into commonjs
-               "sass-loader", //1. Turns sass into css
-            ],
-         },
-      ],
-   },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', //3. Inject styles into DOM
+          'css-loader', //2. Turns css into commonjs
+          'sass-loader', //1. Turns sass into css
+        ],
+      },
+    ],
+  },
 });
