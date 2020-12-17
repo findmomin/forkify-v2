af0d010f504e0a237cbce89c4819f83617bd4786
Abdul Momin <abdulmomin114477@gmail.com>
2020-12-17 16:28:10 +0600

    added config and helper ts

---
+++ src/ts/helpers.ts
@@ -0,0 +1,29 @@
+import * as Interfaces from './models/Interfaces';
+import * as config from './config';
+
+const timeout = (delay: number) => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(new Error(`Request took too long! Timeout after ${delay} second`));
+    }, delay * 1000);
+  });
+};
+
+export const getJSON = async (url: string) => {
+  try {
+    const res = (await Promise.race([
+      fetch(`${url}`),
+      timeout(config.TIMEOUT_SEC),
+    ])) as Response;
+    const data = await res.json();
+
+    if (!res.ok)
+      throw new Error(
+        `${(data as Interfaces.RecipeErrorResponse).message} ${res.status}`
+      );
+
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
